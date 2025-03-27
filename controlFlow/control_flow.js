ac71e82a447a5c74c5a992b7a84046071bc7b972
othmen08 <bensassiothmen@yahoo.fr>
2025-03-27 01:49:53 +0100

    new update

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -52,3 +52,22 @@ let authenticationStatus;
 (isAuthenticated) ? authenticationStatus = "Authenticated" : authenticationStatus = "Not authenticated" ;
 
 console.log("Authentication Staus:",authenticationStatus);
+
+let personRole ="Employee";
+
+
+switch (personRole) {
+    case "Employee":
+        console.log("you are authorized to have acceess to 'Dietary Services'.")
+        break;
+        case "Enrolled Membe":
+            console.log("you are authorized to have access to Dietary Services and one-on-one interaction with a dietician.")
+            break;
+            case "Subscriber":
+                console.log("you are authorized to have partial access to facilitate Dietary Services only.")
+            break;    
+
+    default:
+        console.log("you need to enroll or at least subscribe first to avail this facility.")
+        break;
+}
