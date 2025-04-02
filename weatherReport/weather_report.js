a52be417533cc3d6149cfbcbb4e5e56af4a126b1
othmen08 <bensassiothmen@yahoo.fr>
2025-04-02 11:31:23 +0100

    update

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -11,7 +11,11 @@ function showweatherDetails(event) {
                 weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
                 <p>Temperature: ${data.main.temp} &#8451;</p>
                 <p>Weather: ${data.weather[0].description}</p>`;
-            })
+            }).catch(error => {
+                console.error('Error fetching weather:', error);
+                const weatherInfo = document.getElementById('weatherInfo');
+                weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+              });
 }
 
 document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
